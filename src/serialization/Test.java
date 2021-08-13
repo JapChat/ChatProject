7f89aee8de47412745cfc570ab66614301cfa92e
0206cho <0206cho@naver.com>
2021-08-13 13:24:34 +0900

    민솔 - 직렬화 부분 수정

--- src/serialization/Test.java
+++ src/serialization/Test.java
@@ -11,30 +11,30 @@ public class Test{
 
 	public static void main(String[] args) {
 		Student student = new Student(202045038, "김민솔", "컴퓨터시스템과");
-//		System.out.println(student.getId()+"\t"+student.getName()+"\t"+student.getMajor());
+		System.out.println(student.getId()+"\t"+student.getName()+"\t"+student.getMajor());
 
 		// 직렬화
-//		try {
-//			FileOutputStream fsOut = new FileOutputStream("student.bin");
-//			ObjectOutputStream osOut = new ObjectOutputStream(fsOut);
-//			osOut.writeObject(student);  // 저장
-//			osOut.close();
-//			
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			FileOutputStream fsOut = new FileOutputStream("student.bin");
+			ObjectOutputStream osOut = new ObjectOutputStream(fsOut);
+			osOut.writeObject(student);  // 저장
+			osOut.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		// 역직렬화
 		FileInputStream fsIn;
-		try {
-			fsIn = new FileInputStream("student.bin");
-			ObjectInputStream osIn = new ObjectInputStream(fsIn);
-			Student students = (Student) osIn.readObject();
-			osIn.close();
-			System.out.println(students.getId()+"\t"+students.getName()+"\t"+students.getMajor());
-		} catch (IOException | ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		
+//		try {
+//			fsIn = new FileInputStream("student.bin");
+//			ObjectInputStream osIn = new ObjectInputStream(fsIn);
+//			Student students = (Student) osIn.readObject();
+//			osIn.close();
+//			System.out.println(students.getId()+"\t"+students.getName()+"\t"+students.getMajor());
+//		} catch (IOException | ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}		
 	}
 }
--- src/serialization/Test_2.java
+++ src/serialization/Test_2.java
@@ -11,37 +11,37 @@ public class Test_2 {
 	public static void main(String[] args) {
 		int data = 0;
 		//직렬화
-//        try {
-//            // File 객체 생성
-//            File file = new File("test.txt");
-//            
-//            // 이미 있는 경우 덮어씀
-//            FileOutputStream fos = new FileOutputStream(file);
-//            while((data = System.in.read()) != -1) {
-//                // 입력받은 내용을 파일 내용으로 기록
-//                fos.write(data);
-//            }
-//            fos.close(); // 파일을 닫기
-//        } catch (FileNotFoundException e) {  //FileOutputStream 객체 생성시 파일 경로가 유효하지 않으면 발생           
-//            e.printStackTrace();
-//        } catch (IOException e) { //read, write
-//			e.printStackTrace();
-//		} 
-	
-
-		// 역직렬화
         try {
-        	File file = new File("test.txt");
-            FileInputStream fis = new FileInputStream(file);
-            while((data = fis.read()) != -1) {
+            // File 객체 생성
+            File file = new File("test.txt");
+            
+            // 이미 있는 경우 덮어씀
+            FileOutputStream fos = new FileOutputStream(file);
+            while((data = System.in.read()) != -1) {
                 // 입력받은 내용을 파일 내용으로 기록
-                System.out.write(data);
+                fos.write(data);
             }
-            fis.close(); // 파일을 닫기
+            fos.close(); // 파일을 닫기
         } catch (FileNotFoundException e) {  //FileOutputStream 객체 생성시 파일 경로가 유효하지 않으면 발생           
             e.printStackTrace();
         } catch (IOException e) { //read, write
 			e.printStackTrace();
 		} 
+	
+
+		// 역직렬화
+//        try {
+//        	File file = new File("test.txt");
+//            FileInputStream fis = new FileInputStream(file);
+//            while((data = fis.read()) != -1) {
+//                // 입력받은 내용을 파일 내용으로 기록
+//                System.out.write(data);
+//            }
+//            fis.close(); // 파일을 닫기
+//        } catch (FileNotFoundException e) {  //FileOutputStream 객체 생성시 파일 경로가 유효하지 않으면 발생           
+//            e.printStackTrace();
+//        } catch (IOException e) { //read, write
+//			e.printStackTrace();
+//		} 
     }
 }
